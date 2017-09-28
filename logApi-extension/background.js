d30b6ee4fb0a0ae38691891909d04bd3321f77bc
Anchit Agarwal <anchit008@gmail.com>
2017-09-28 14:05:50 -0700

    Updated doc

--- logApi-extension/background.js
+++ logApi-extension/background.js
@@ -2,14 +2,16 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         var username = '';
         $.ajax({
-            url: 'http://127.0.0.1:3000/user_data',
+            // url: 'http://127.0.0.1:3000/user_data',
+            url: 'http://54.67.1.228/user_data',
             type: 'get',
             dataType: 'json',
             success: function (data) {
                 username = data.username
                 request.user = username
                 $.ajax({
-                    url: 'http://127.0.0.1:3000/log',
+                    // url: 'http://127.0.0.1:3000/log',
+                    url: 'http://54.67.1.228/log',
                     type: 'post',
                     dataType: 'json',
                     data: request,
